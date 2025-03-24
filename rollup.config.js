a189148aeece5cbb704f60fe329dc72f08232c00
2100 <wbzeqy@163.com>
2025-03-24 15:44:12 +0800

    chore: 添加依赖、build逻辑

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,62 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import strip from '@rollup/plugin-strip';
+import json from '@rollup/plugin-json';
+
+const pkg = require('./package.json');
+const { name, version, description, author, license } = pkg;
+const banner = `/*!
+* ${name} v${version}
+* ${description}
+* (c) ${new Date().getFullYear()} ${author}
+* Released under the ${license} License.
+*/`
+
 const formatMap = {
   esm: {
     file: 'dist/es/index.js',
     format: 'esm',
+    banner,
   },
   cjs: {
     file: 'dist/lib/index.js',
     format: 'cjs',
+    banner,
   }
 }
 const createConfig = output => {
   return {
     input: 'src/index.js',
     output,
-    plugins: [],
-    external: ['vue'],
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelrc: false,
+        exclude: 'node_modules/**', // 只转译我们的源代码
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: {
+                browsers: ['> 1%', 'last 2 versions', 'ie >= 11']
+              }
+            }
+          ]
+        ],
+      }),
+      vue(),
+      commonjs(),
+      terser(),
+      strip({
+        labels: ['unittest'],
+      }),
+      json(),
+    ],
+    external: ['vue', 'xe-utils'],
   }
 }
-const config = Object.values(formatMap).values.map(output => createConfig(output))
-module.export = config
-// pnpm add @rollup/plugin-json @rollup/plugin-terser @rollup/plugin-node-resolve @rollup/plugin-babel rollup-plugin-vue @rollup/plugin-strip -D -w
+const config = Object.values(formatMap).map(output => createConfig(output))
+module.exports = config
