be9468e231ce14f9256f218670cb18bcbea7dfda
2100 <wbzeqy@163.com>
2025-03-25 14:15:26 +0800

    chore: 添加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { parser: 'flow' }],
+    'no-prototype-builtins': 0,
+  },
+};
