3d1ecfac989bf886ea15be680f5c5fe50a55eaac
2100 <wbzeqy@163.com>
2025-03-30 00:35:36 +0800

    docs: :memo: 创建问价结构

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  base: '/docs/',
+  base: '/docs-vxe-table-middleware/',
+  title: 'VxeTableMiddleware',
+  description: '应用vxe-table时的一个中间件，提供一个创建表格和配置的方案，让Vue2开发的项目支持一定的类型提示，提升开发体验',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   port: 9527,
   dest: 'dist',
   locales: {
@@ -8,5 +14,23 @@ module.exports = {
       title: 'vxe-table-middleware',
       description: 'VxeTable表格中间件说明文档',
     },
+  },
+  themeConfig: {
+    logo: '/avatar.png',
+    author: '2100',
+    locales: {
+      '/zh/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        nav: [],
+        sidebar: {
+          '/zh/': [
+            'about'
+          ]
+        },
+        lastUpdated: 'Last Updated',
+        smoothScroll: true
+      }
+    }
   }
 }
--- docs/src/README.md
+++ docs/src/README.md
@@ -1,11 +1,17 @@
 ---
 home: true
+heroImage: /avatar.png
 heroText: VxeTableMiddleware
 tagline: VxeTable表格中间件说明文档
 actionText: 快速上手 →
+actionLink: /zh/about/
 features:
   - title: 类型提示
     details: Vue2开发场景实现TypeScript类型提示方案，提升开发体验和效率
+  - title: 渐进式
+    details: 仅是中间件工具，随装随用，不影响原使用
+  - title: 拓展
+    details: 基于VxeTable拓展功能
 ---
 
 ::: slot footer
