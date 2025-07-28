8bd1a7b997f3f7a6447c1843e1827b1f029947b9
2100 <wbzeqy@163.com>
2025-07-29 00:44:31 +0800

    chore: :hammer: 编辑注释

--- types/extend-helper.d.ts
+++ types/extend-helper.d.ts
@@ -1,6 +1,7 @@
 import type { OptionsHelperIns } from './helpers/options';
 import type { ColumnsHelperIns } from './helpers/columns';
 
+// #region extendHelperType
 type HelperType = 'option' | 'column';
 type InstanceType<T> = T extends 'option'
   ? OptionsHelperIns
@@ -15,3 +16,4 @@ export type HelpersDecoratorHandler = <T extends HelperType>(
   type: T,
   extension: ExtensionMethods<T>
 ) => void;
+// #endregion extendHelperType
